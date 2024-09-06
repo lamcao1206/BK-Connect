54023d1d069701ae54fe015e1fd13f8572356d8d
Cao Ngoc Lam <mrhuman2004@gmail.com>
2024-09-06 10:24:48 +0700

    refactor the error handler

--- server/middleware/error.middleware.js
+++ server/middleware/error.middleware.js
@@ -1,12 +1,14 @@
 import handleError from "../utils/error.handle.js";
 
 const errorMiddleware = (err, req, res, next) => {
-  try {
-    const errors = handleError(err);
-    res.status(500).json({ errors });
-  } catch (err) {
-    next(err);
-  }
+  const { errors, statusCode } = handleError(err);
+  return res.status(statusCode).json({
+    message: err.message || "Some error occurred",
+    status: statusCode,
+    errors,
+    timestamp: new Date().toISOString(),
+    path: req.originalUrl,
+  });
 };
 
 export default errorMiddleware;
--- server/utils/error.handle.js
+++ server/utils/error.handle.js
@@ -1,14 +1,16 @@
 const handleError = (err) => {
   let errors = {};
+  let statusCode = 500;
 
   // Contain duplicate value in username of email
   if (err.code === 11000) {
+    statusCode = 409;
     Object.entries(err.keyValue).forEach(([field, value]) => {
       errors[field] = value + " already exists";
     });
   }
 
-  return errors;
+  return { errors, statusCode };
 };
 
 export default handleError;
